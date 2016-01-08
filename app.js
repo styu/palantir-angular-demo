21dfdbf8531390b601437a182e6d9b7bfd4ade34
Stephanie Yu <stephaniey@palantir.com>
2016-01-08 14:30:36 -0500

    Angular Demo (complete)

---
+++ app.js
@@ -0,0 +1,119 @@
+// global variables
+var xPiece = "X";
+var oPiece = "O";
+
+// empty template game for when we start a new game
+var templateGame = [
+  [ { value: null }, { value: null }, { value: null }],
+  [ { value: null }, { value: null }, { value: null }],
+  [ { value: null }, { value: null }, { value: null }]
+];
+
+// creates the angular application
+var app = angular.module('ticTacToeApp', []);
+
+// creates the angular directive
+// notice that the directive name is ticTacToe, but in HTML you will reference the tag as <tic-tac-toe></tic-tac-toe>
+app.directive("ticTacToe", function() {
+  // controller for the directive
+  var ticTacToeController = function($window, $scope) {
+    // to avoid conflicts with 'this' in callback functions
+    var controller = this;
+
+    this.currentGame = null;
+    this.currentPlayer = null;
+    this.winningPlayer = null;
+    this.tied = false;
+
+    // marks the board with the piece of the current player
+    controller.markBoard = function(row, column) {
+      // check that the game isn't over and that the cell isn't already marked
+      if (controller.currentGame[row][column].value == null && controller.winningPlayer == null && !controller.tied) {
+        controller.currentGame[row][column].value = controller.currentPlayer;
+        controller.switchPlayer();
+
+        // check for winner
+        var winningMoves = checkForWinner(controller.currentGame);
+        if (winningMoves.tied) {
+          controller.tied = true;
+        } else {
+          // we need to mark each cell as a winning cell so we can color it green
+          winningMoves.forEach(function(winningMove) {
+            controller.recordWin(winningMove.player, winningMove.type, winningMove.index);
+          });
+          controller.winningPlayer = winner;
+        }
+      }
+    }
+
+    // marks the cells as a winning cell based on the row/column
+    controller.recordWin = function(winner, type, index) {
+      switch(type) {
+        case "row":
+          for (var col = 0; col < 3; col++) {
+            controller.currentGame[index][col].winner = true;
+          }
+          break;
+        case "column":
+          for (var row = 0; row < 3; row++) {
+            controller.currentGame[row][index].winner = true;
+          }
+          break;
+        case "diagonal":
+          if (index === 0) {
+            for (var i = 0; i < 3; i++) {
+              controller.currentGame[i][i].winner = true;
+            }
+          } else {
+            for (var i = 2; i >= 0; i--) {
+              controller.currentGame[i][2 - i].winner = true;
+            }
+          }
+          break;
+      }
+    }
+
+    // starts a new game
+    controller.startNewGame = function() {
+      // makes a deep copy of the template game
+      controller.currentGame = angular.copy(templateGame);
+      // resets the winner
+      controller.winningPlayer = null;
+      // switches the player if we've already been playing
+      if (controller.currentPlayer != null) {
+        controller.switchPlayer();
+      } else {
+        controller.currentPlayer = oPiece;
+      }
+    }
+
+    // toggles the player between X and O, depending on what the current player is
+    controller.switchPlayer = function() {
+      if (controller.currentPlayer === xPiece) {
+        controller.currentPlayer = oPiece;
+      } else {
+        controller.currentPlayer = xPiece;
+      }
+    }
+  };
+
+  return {
+    controller: ticTacToeController,
+    controllerAs: "controller", // this is how the controller will be referenced in the directive
+    scope: true,
+    // Using ` allows us to have a string that spans multiple lines!
+    template: `<button ng-click="controller.startNewGame()">Start New Game</button>
+      <div ng-if="controller.currentGame" class='game-container'>
+        <div class="player">Current Player: {{controller.currentPlayer}}</div>
+        <div class="winning-player" ng-if="controller.winningPlayer">Winner: {{controller.winningPlayer}}</div>
+        <div class="tied" ng-if="controller.tied">Tied!</div>
+        <div class="game">
+          <div class="row" ng-repeat="row in controller.currentGame">
+            <div class="cell" ng-class="{ winner: cell.winner }" ng-repeat="cell in row" ng-click="controller.markBoard($parent.$index, $index)">
+              <span ng-if="cell.value">{{cell.value}}</span>
+            </div>
+          </div>
+        </div>
+      </div>`
+  };
+});
---
+++ tictactoe.js
@@ -0,0 +1,90 @@
+// Given a tic tac toe game (in the form of a 2D array), it checks for
+// winners
+// if there is a winner (could have multiple winning rows), it returns an array of all the winning rows and the player that one
+// if there is not a winner, it sends back an empty array
+// if there is a tie, it sends back { tied: true }
+function checkForWinner(game) {
+  var xPiece = "X";
+  var oPiece = "O";
+
+  var winningMoves = [];
+  for (var row = 0; row < game.length; row++) {
+    var isX = game[row].every(function(val) {
+      return val.value === xPiece;
+    });
+
+    if (isX) {
+      winningMoves.push({
+        player: xPiece,
+        type: "row",
+        index: row
+      });
+    };
+
+    var isO = game[row].every(function(val) {
+      return val.value === oPiece;
+    });
+    if (isO) {
+      winningMoves.push({
+        player: oPiece,
+        type: "row",
+        index: row
+      });
+    };
+  }
+
+  // check columns
+  for (var col = 0; col < game.length; col++) {
+    var isX = game.every(function(row) {
+      return row[col].value === xPiece;
+    });
+
+    if (isX) {
+      winningMoves.push({
+        player: xPiece,
+        type: "column",
+        index: col
+      });
+    }
+
+    var isO = game.every(function(row) {
+      return row[col].value === oPiece;
+    });
+
+    if (isO) {
+      winningMoves.push({
+        player: oPiece,
+        type: "column",
+        index: col
+      });
+    }
+  }
+
+  // check diagonals
+  var game = game;
+  if (game[0][0].value != null && game[0][0].value === game[1][1].value && game[1][1].value === game[2][2].value) {
+    winningMoves.push({
+      player: game[1][1].value,
+      type: "diagonal",
+      index: 0
+    });
+  } else if (game[2][0].value != null && game[2][0].value === game[1][1].value && game[1][1].value === game[0][2].value) {
+    winningMoves.push({
+      player: game[1][1].value,
+      type: "diagonal",
+      index: 1
+    });
+  }
+
+  var tied = game.every(function(row) {
+    return row.every(function(cell) {
+      return cell.value != null;
+    });
+  })
+
+  if (tied) {
+    return { tied: true };
+  } else {
+    return winningMoves;
+  }
+}
